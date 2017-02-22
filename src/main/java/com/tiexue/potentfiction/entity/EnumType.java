bd699638bd961837b65782f085b27b44d2ad6b6c
zhangxiaowei <zhangxiaowei@tiexue.com>
2017-02-22 09:48:58 +0800

    修改枚举为常量

--- src/main/java/com/tiexue/potentfiction/entity/EnumType.java
+++ src/main/java/com/tiexue/potentfiction/entity/EnumType.java
@@ -1,354 +1,121 @@
 package com.tiexue.potentfiction.entity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class EnumType {
+	// 小说状态常量
+    public static final int BookStatus_Offline = 0;
+    public static final int BookStatus_Update = 1;
+    public static final int BookStatus_Finish = 2;
+    public static final int BookStatus_Delete = 3;
+    //小说状态
+    public static Map<Integer, String> BookStatus = new HashMap<Integer,String>(){
+	{
+    	put(BookStatus_Offline, "未上线");
+    	put(BookStatus_Update, "连载中");
+    	put(BookStatus_Finish, "完结");
+    	put(BookStatus_Delete, "删除");
+    }};
 
-	// 小说状态
-	public enum BookStatus {
-		NOTONLINE("未上线", 0), UPDATE("连载中", 1), FINISH("完结", 2),DELETE("删除",3);
-		// 成员变量
-		private String name;
-		private int index;
+    //小说标识常量
+    public static final int BookMark_Normal=0;
+    public static final int BookMark_Recomment=1;
+    public static final int BookMark_Hot=2;
+    public static final int BookMark_Essence=3;
+    //小说标识
+    public static Map<Integer,String> BookMark=new HashMap<Integer,String>(){
+    	{
+    		put(BookMark_Normal,"正常");
+    		put(BookMark_Recomment,"推荐");
+    		put(BookMark_Hot,"热门");
+    		put(BookMark_Essence,"精华");
+    	}
+    };
+    	
+    //章节类型常量
+    public static final int ChapterType_Free=0;
+    public static final int ChapterType_Pay=1;
+    //章节类型
+    public static Map<Integer,String> ChapterType=new HashMap<Integer,String>(){
+    	{
+    		put(ChapterType_Free,"免费");
+    		put(ChapterType_Pay,"付费");
+    	}
+    };
 
-		// 构造方法
-		private BookStatus(String name, int index) {
-			this.name = name;
-			this.index = index;
-		}
+    //章节状态常量
+    public static final int ChapterStatus_Offline=0;
+    public static final int ChapterStatus_OnLine=1;
+    public static final int ChapterStatus_Delete=2;
+    //章节状态
+    public static Map<Integer,String> ChapterStatus=new HashMap<Integer,String>(){
+    	{
+    		put(ChapterStatus_Offline,"未上线");
+    		put(ChapterStatus_OnLine,"删除");
+    		put(ChapterStatus_Delete,"上线");
+    	}
+    };
 
-		// 普通方法
-		public static String getName(int index) {
-			for (BookStatus c : BookStatus.values()) {
-				if (c.getIndex() == index) {
-					return c.name;
-				}
-			}
-			return null;
-		}
+    //评论状态常量
+    public static final int CommentStatus_Normal=0;
+    public static final int CommentStatus_Delete=1;
+    //评论状态
+    public static Map<Integer,String> CommentStatus=new HashMap<Integer,String>(){
+    	{
+    		put(CommentStatus_Normal,"正常");
+    		put(CommentStatus_Delete,"删除");
+    	}
+    };
 
-		public String getName() {
-			return name;
-		}
+    //充值类型常量
+    public static final int PayType_NovelCurr=0;
+    public static final int PayType_Time=1;
+    //充值类型
+    public static Map<Integer,String> PayType=new HashMap<Integer,String>(){
+    	{
+    		put(PayType_NovelCurr,"小说币");
+    		put(PayType_Time,"包月(年)");
+    	}
+    };
 
-		public void setName(String name) {
-			this.name = name;
-		}
 
-		public int getIndex() {
-			return index;
-		}
+    //包年(月)充值的单位常量
+    public static final int PayUnit_Year=0;
+    public static final int PayUnit_Month=1;
+    public static final int PayUnit_Quarter=2;
+    //包年(月)充值的单位
+    public static Map<Integer,String> PayUnit=new HashMap<Integer,String>(){
+    	{
+    		put(PayUnit_Year,"年");
+    		put(PayUnit_Month,"月");
+    		put(PayUnit_Quarter,"季度");
+    	}
+    };
+    
+    //用户类型常量
+    public static final int UserType_Normal=0;
+    public static final int UserType_VIP=1;
+    //用户类型
+    public static Map<Integer,String> UserType=new HashMap<Integer,String>(){
+    	{
+    		put(UserType_Normal,"普通用户");
+    		put(UserType_VIP,"VIP用户");
+    	}
+    };
 
-		public void setIndex(int index) {
-			this.index = index;
-		}
+    //用户状态常量
+    public static final int UserStatus_Normal=0;
+    public static final int UserStatus_Delete=1;
+    //用户状态
+    public static Map<Integer,String> UserStatus=new HashMap<Integer,String>(){
+    	{
+    		put(UserStatus_Normal,"正常");
+    		put(UserStatus_Delete,"删除");
+    	}
+    };
 
-	}
-
-	// 小说标识
-	public enum BookMark {
-		NORMAL("正常", 0),RECOMMENT("推荐",1),HOT("热门",2),ESSENCE("精华",3);
-		// 成员变量
-		private String name;
-		private int index;
-
-		// 构造方法
-		private BookMark(String name, int index) {
-			this.name = name;
-			this.index = index;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public int getIndex() {
-			return index;
-		}
-
-		public void setIndex(int index) {
-			this.index = index;
-		}
 		
-		public static String getName(int index)
-		{
-			for (BookMark c : BookMark.values()) {
-				if(c.getIndex()==index){
-					return c.name;
-				}
-			}
-			return "";
-		}
 
-	}
-	
-	//章节类型
-	public enum ChapterType{
-		FREE("免费",0),PAY("付费",1);
-		private String name;
-		private int index;
-		private ChapterType(String name,int index)
-		{
-			this.name=name;
-			this.index=index;
-		}
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public int getIndex() {
-			return index;
-		}
-		public void setIndex(int index) {
-			this.index = index;
-		}
-		public static String getName(int index)
-		{
-			for(ChapterType c :ChapterType.values()){
-				if(c.getIndex()==index){
-					return c.name;
-				}
-			}
-			return null;
-		}
-	}
-	
-	//章节状态
-	public enum ChapterStatus{
-		
-		NOTONLINE("未上线",0),DELETE("删除",1),ONLINE("上线",2);
-		
-		private String name;
-		private int index;
-		
-		private ChapterStatus(String name,int index){
-			this.name=name;
-			this.index=index;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public int getIndex() {
-			return index;
-		}
-
-		public void setIndex(int index) {
-			this.index = index;
-		}
-		
-		public static String getName(int index){
-			for(ChapterStatus c:ChapterStatus.values()){
-				if(c.getIndex()==index){
-					return c.name;
-				}
-			}
-			return null;
-		}
-	}
-	
-	
-
-	//评论状态
-	public enum CommentStatus{
-		
-		NORMAL("正常",0),DELETE("删除",1);
-		
-		private String name;
-		private int index;
-		
-		private CommentStatus(String name,int index){
-			this.name=name;
-			this.index=index;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public int getIndex() {
-			return index;
-		}
-
-		public void setIndex(int index) {
-			this.index = index;
-		}
-		
-		public static String getName(int index){
-			for(CommentStatus c:CommentStatus.values()){
-				if(c.getIndex()==index){
-					return c.name;
-				}
-			}
-			return null;
-		}
-	}
-	
-	// 充值类型
-	public enum PayType {
-
-		NOVELCURR("小说币", 0), TIME("包月(年)", 1);
-
-		private String name;
-		private int index;
-
-		private PayType(String name, int index) {
-			this.name = name;
-			this.index = index;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public int getIndex() {
-			return index;
-		}
-
-		public void setIndex(int index) {
-			this.index = index;
-		}
-
-		public static String getName(int index) {
-			for (PayType c : PayType.values()) {
-				if (c.getIndex() == index) {
-					return c.name;
-				}
-			}
-			return null;
-		}
-	}
-		
-	//包年(月)充值的单位
-	public enum PayUnit {
-
-		YEAR("", 0), MONTH("月", 1),QUARTER("季度",2);
-
-		private String name;
-		private int index;
-
-		private PayUnit(String name, int index) {
-			this.name = name;
-			this.index = index;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public int getIndex() {
-			return index;
-		}
-
-		public void setIndex(int index) {
-			this.index = index;
-		}
-
-		public static String getName(int index) {
-			for (PayUnit c : PayUnit.values()) {
-				if (c.getIndex() == index) {
-					return c.name;
-				}
-			}
-			return null;
-		}
-	}
-	
-	// 用户类型
-	public enum UserType {
-
-		NORMAL("普通用户", 0), VIP("VIP用户", 1);
-		private String name;
-		private int index;
-
-		private UserType(String name, int index) {
-			this.name = name;
-			this.index = index;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public int getIndex() {
-			return index;
-		}
-
-		public void setIndex(int index) {
-			this.index = index;
-		}
-
-		public static String getName(int index) {
-			for (UserType c : UserType.values()) {
-				if (c.getIndex() == index) {
-					return c.name;
-				}
-			}
-			return null;
-		}
-	}
-			
-	// 用户类型
-	public enum UserStatus {
-
-		NORMAL("正常", 0), DELETE("删除", 1);
-		private String name;
-		private int index;
-
-		private UserStatus(String name, int index) {
-			this.name = name;
-			this.index = index;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public int getIndex() {
-			return index;
-		}
-
-		public void setIndex(int index) {
-			this.index = index;
-		}
-
-		public static String getName(int index) {
-			for (UserStatus c : UserStatus.values()) {
-				if (c.getIndex() == index) {
-					return c.name;
-				}
-			}
-			return null;
-		}
-	}
 			
 }
--- src/test/java/com/tiexue/potentfiction/test/EnumTypeTest.java
+++ src/test/java/com/tiexue/potentfiction/test/EnumTypeTest.java
@@ -12,42 +12,45 @@ public class EnumTypeTest {
 	@Test
 	public void TestEnum()
 	{
-	    System.out.println("小说状态");
-		for (EnumType.BookStatus item : EnumType.BookStatus.values()) {
-			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
-		}
-		System.out.println("小说标识");
-		for (EnumType.BookMark item : EnumType.BookMark.values()) {
-			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
-		}
-		System.out.println("章节状态");
-		for (EnumType.ChapterStatus item : EnumType.ChapterStatus.values()) {
-			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
-		}
-		System.out.println("章节类型");
-		for (EnumType.ChapterType item : EnumType.ChapterType.values()) {
-			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
-		}
-		System.out.println("评论状态");
-		for (EnumType.CommentStatus item : EnumType.CommentStatus.values()) {
-			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
-		}
-		System.out.println("支付类型");
-		for (EnumType.PayType item : EnumType.PayType.values()) {
-			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
-		}
-		System.out.println("支付单位");
-		for (EnumType.PayUnit item : EnumType.PayUnit.values()) {
-			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
-		}
-		System.out.println("用户状态");
-		for (EnumType.UserStatus item : EnumType.UserStatus.values()) {
-			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
-		}
-		System.out.println("用户类型");
-		for (EnumType.UserType item : EnumType.UserType.values()) {
-			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
-		}
+		System.out.println("小说状态");
+		System.out.println(EnumType.BookMark_Essence);
+		System.out.println(EnumType.BookMark.get(EnumType.BookMark_Essence));
+//	    System.out.println("小说状态");
+//		for (EnumType.BookStatus item : EnumType.BookStatus.values()) {
+//			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
+//		}
+//		System.out.println("小说标识");
+//		for (EnumType.BookMark item : EnumType.BookMark.values()) {
+//			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
+//		}
+//		System.out.println("章节状态");
+//		for (EnumType.ChapterStatus item : EnumType.ChapterStatus.values()) {
+//			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
+//		}
+//		System.out.println("章节类型");
+//		for (EnumType.ChapterType item : EnumType.ChapterType.values()) {
+//			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
+//		}
+//		System.out.println("评论状态");
+//		for (EnumType.CommentStatus item : EnumType.CommentStatus.values()) {
+//			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
+//		}
+//		System.out.println("支付类型");
+//		for (EnumType.PayType item : EnumType.PayType.values()) {
+//			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
+//		}
+//		System.out.println("支付单位");
+//		for (EnumType.PayUnit item : EnumType.PayUnit.values()) {
+//			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
+//		}
+//		System.out.println("用户状态");
+//		for (EnumType.UserStatus item : EnumType.UserStatus.values()) {
+//			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
+//		}
+//		System.out.println("用户类型");
+//		for (EnumType.UserType item : EnumType.UserType.values()) {
+//			System.out.println("index:"+item.getIndex()+".  name:"+item.getName());
+//		}
 	}
 
 }
