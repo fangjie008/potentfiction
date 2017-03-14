aa4588bf1a9150eeb1adfd4422756408bba6a35a
fangjie008 <fangjie008@163.com>
2017-03-14 10:07:59 +0800

    2

--- src/main/java/com/tiexue/potentfiction/entity/WxConstants.java
+++ src/main/java/com/tiexue/potentfiction/entity/WxConstants.java
@@ -20,10 +20,10 @@ public class WxConstants {
 	public static final String WxSnsLang = "zh_CN";
 
 	// 微信商户支付相关
-	public static final String WxMch_Id = "";
+	public static final String WxMch_Id = "1448907102";
 
 	// 微信商户支付秘钥
-	public static final String WxMch_Key = "";
+	public static final String WxMch_Key = "t0EZVIvBwvytVL4RYh2H0HuR6sygnDxv";
 
 	// 微信商户支付后回调url
 	public static final String WxMch_NotifyUrl = "http://wx.top657.cn/wxPay/wxpaycallback";
